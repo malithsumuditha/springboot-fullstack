107415e99a2409c148c70aa29c449ce754fec522
MalithU <malith.udara@synapsys.lk>
2023-11-13 20:09:00 +0530

    testing cd from branch test1

--- backend/src/main/java/com/malith/mysystem/controller/TestController.java
+++ backend/src/main/java/com/malith/mysystem/controller/TestController.java
@@ -9,6 +9,6 @@ public class TestController {
 
     @GetMapping("/malith")
     public Malith getString(){
-        return new Malith("Test Success again...555");
+        return new Malith("Test Success again...5559");
     }
 }
