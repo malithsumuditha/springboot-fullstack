6f1744e47ec1ef9583df1a4372f440eb12856214
MalithU <malith.udara@synapsys.lk>
2023-11-13 22:10:52 +0530

    test profile

--- backend/src/main/java/com/malith/mysystem/controller/TestController.java
+++ backend/src/main/java/com/malith/mysystem/controller/TestController.java
@@ -9,6 +9,6 @@ public class TestController {
 
     @GetMapping("/malith")
     public Malith getString(){
-        return new Malith("Hello CI CD malith... HIII");
+        return new Malith("Hello CI CD malith... HIIIw");
     }
 }
