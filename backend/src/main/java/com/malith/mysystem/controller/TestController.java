51cf9e19ec8c84b39ab9b971e10a75b9f2a17a30
MalithU <malith.udara@synapsys.lk>
2023-11-13 23:59:07 +0530

    Slack messages config all test

--- backend/src/main/java/com/malith/mysystem/controller/TestController.java
+++ backend/src/main/java/com/malith/mysystem/controller/TestController.java
@@ -9,6 +9,6 @@ public class TestController {
 
     @GetMapping("/malith")
     public Malith getString(){
-        return new Malith("Slack test success...");
+        return new Malith("Slack test success");
     }
 }
